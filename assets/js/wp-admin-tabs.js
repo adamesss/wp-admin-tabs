63dabf793249c67c4ce1642dbf0f08b441d126d2
ruslangallo <ruslangallo.inside@gmail.com>
2016-10-27 11:57:54 +0300

    Add function for each block

--- assets/js/wp-admin-tabs.js
+++ assets/js/wp-admin-tabs.js
@@ -1,23 +1,28 @@
 // WP Admin Tabs
 
 jQuery(document).ready(function($){
+	// Variables
 	var tabContainer = $('.tab-container');
 
-	tabContainer.addClass('tab-container-js').removeClass('tab-container-without-js');
-    tabContainer.find('.nav-tab-wrapper a').click(function(e) {
-		e.preventDefault();
-        var link		= $(this),
-			tabId		= link.attr('href'),
-			tab 		= $(tabId);
+	tabContainer.each(function(){
+		var tabContainer = $(this);
+		
+		tabContainer.addClass('tab-container-js').removeClass('tab-container-without-js');
+	    tabContainer.find('.nav-tab-wrapper a').click(function(e) {
+			e.preventDefault();
+	        var link		= $(this),
+				tabId		= link.attr('href'),
+				tab 		= $(tabId);
 
-        // Active tab
-        link.siblings().removeClass('nav-tab-active');
-        link.addClass('nav-tab-active');
+	        // Active tab
+	        link.siblings().removeClass('nav-tab-active');
+	        link.addClass('nav-tab-active');
 
-        // Active tab content
-		tab.siblings().addClass('hidden');
-		tab.removeClass('hidden');
+	        // Active tab content
+			tab.siblings().addClass('hidden');
+			tab.removeClass('hidden');
 
-		return false;
-    });
+			return false;
+	    });
+	});
 });
